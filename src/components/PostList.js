424e9ff8307192603f943e6ce21fac5cf09d7c9e
JeongHwanjun <ghkswnsdlwjd@naver.com>
2024-11-20 15:18:20 +0900

    Scroll complete

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,5 @@
 import PostListItem from "./PostListItem"
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useRef} from 'react'
 import { AutoSizer, List } from "react-virtualized"
 import axios from "axios"
 import '../styles/Styles.scss'
@@ -8,39 +8,43 @@ const PostList = () => {
     const [loading, setLoading] = useState(false)
     const [posts, setPosts] = useState([])
     const [page, setPage] = useState(1)
+    const scrollPositionRef = useRef(0);
+    const listRef = useRef(null)
 
     useEffect(() => {
         const fetchPost = async () => { // 데이터를 받아옴
             setLoading(true)
             try{
-                const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+                // 로딩 과정을 알 수 있게 한번에 소량만 받아옴
+                const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`)
                 setPosts(prevPosts => [...prevPosts, ...res.data])
             } catch (e) {
                 console.log(e)
             }
             setLoading(false)
+            console.log(posts.length)
         }
         fetchPost()
     },[page])
-
-    if(loading){
-        return (
-            <div>로딩중</div>
-        )
-    }
-
-    if(!posts){
-        return null
-    }
+    
 
     const loadingNextPage = ({scrollTop, clientHeight, scrollHeight}) => {
+        scrollPositionRef.current = scrollTop
         if(scrollHeight - scrollTop <= 1.5 * clientHeight && !loading){
             setPage(prevPage => prevPage + 1)
+            console.log("reload")
         }
     }
 
+    useEffect(() => {
+        if(listRef.current){
+            listRef.current.scrollToPosition(scrollPositionRef.current)
+        }
+    })
+
     const rowRenderer = ({index, key, style}) =>{
         const post = posts[index]
+        if(!post) return null
         return(
             <PostListItem
             post={post}
@@ -50,7 +54,10 @@ const PostList = () => {
     }
 
     return(
+        <div>
+            {loading && posts.length === 0 && <div>로딩중 . . . </div>}
             <List
+                ref={listRef}
                 className="PostList"
                 width={768}
                 height={500}
@@ -60,6 +67,9 @@ const PostList = () => {
                 onScroll={loadingNextPage}
                 style={{outline : 'none'}}
             />
+            {loading && posts.length>0 && <div>로딩중 . . . </div>}
+        </div>
+            
     )
 }
 
